5166f7c2bfd0520d5d448207dc0262fef2f46a0e
kinan.karam92 <kinan.karam92@gmail.com>
2018-01-30 10:21:40 +0100

    Add project files.

---
+++ WebAppId/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppId.Startup))]
+namespace WebAppId
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
